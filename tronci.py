8b74806f552896bf14cf2b2345e73a932802697f
Fabio Menegazzo <menegazzo@esss.com.br>
2014-06-06 16:49:30 -0300

    Starting jobs list.

--- tronci.py
+++ tronci.py
@@ -1,13 +1,20 @@
+from apscheduler.jobstores.sqlalchemy_store import SQLAlchemyJobStore
+from apscheduler.scheduler import Scheduler
 from flask import Flask, render_template_string, session, redirect, url_for, render_template
 from flask.globals import g, request
 from flask_github import GitHub
 from sqlalchemy import Column, Integer, Sequence, String
 from sqlalchemy.engine import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.orm.scoping import scoped_session
 from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.sql.schema import ForeignKey, MetaData
 from travispy import TravisPy
 import config
+
+
+# Settings -----------------------------------------------------------------------------------------
 
 # Flask
 app = Flask(__name__)
@@ -19,9 +26,18 @@ github = GitHub(app)
 # SQLAlchemy
 engine = create_engine(config.DATABASE_URI, echo=True)
 db_session = scoped_session(sessionmaker(engine, autoflush=False, autocommit=False))
+metadata = MetaData(engine)
 
-Base = declarative_base()
+Base = declarative_base(engine, metadata)
 Base.query = db_session.query_property()
+
+# APScheduler
+job_store = SQLAlchemyJobStore(engine=engine, metadata=metadata)
+scheduler = Scheduler()
+scheduler.add_jobstore(job_store, 'db')
+
+
+# Database -----------------------------------------------------------------------------------------
 
 def init_db():
     Base.metadata.create_all(engine)
@@ -46,6 +62,24 @@ class User(Base):
         )
 
 
+class APSJob(Base):
+
+    __table__ = job_store.jobs_t
+
+    job = relationship('Job', order_by='Job.id', backref='job')
+
+
+class Job(Base):
+
+    __tablename__ = 'jobs'
+
+    id = Column(Integer, Sequence('job_id_seq'), primary_key=True)
+    job_id = Column(Integer, ForeignKey('apscheduler_jobs.id'))
+    repository_slug = Column(String(500))
+
+    aps_job = relationship('APSJob', backref=backref('aps_job', order_by=id))
+
+
 # App ----------------------------------------------------------------------------------------------
 
 @app.before_request
@@ -65,12 +99,26 @@ def after_request(response):
 def index():
     user = g.user
     if user:
+        return redirect(url_for('repositories'))
+    else:
+        return render_template_string('Hello! <a href="{{ url_for("login") }}">Login</a>')
+
+
+@app.route('/repositories')
+def repositories():
+    user = g.user
+    if user:
         travispy = TravisPy.github_auth(user.github_access_token)
         github_user = travispy.user()
         repos = travispy.repos(member=github_user.login, active=True)
         return render_template('repositories.html', user=github_user, repos=repos)
     else:
-        return render_template_string('Hello! <a href="{{ url_for("login") }}">Login</a>')
+        return redirect(url_for('login'))
+
+
+@app.route('/jobs/<path:slug>')
+def jobs(slug):
+    return slug
 
 
 @app.route('/login')

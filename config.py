60987c031aefe0ae400435a15100835a40df41a5
Fabio Menegazzo <menegazzo@esss.com.br>
2014-06-24 13:13:43 -0300

    .

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ SECRET_KEY = 'DVsItM4vYgvRZd6G/htFOR/+hGgOjCPPrjdg+6hFZQ6E93r9aivYXyYWC3DnaHU48/
 # GitHub variables
 GITHUB_CLIENT_ID = 'e959975a5bd3e54a2145'
 GITHUB_CLIENT_SECRET = 'd0f0deeca1a891c22bb679385e215319d4ad66db'
-GITHUB_CALLBACK_URL = 'http://tron-ci.herokuapp.com:5000/github-callback'
+GITHUB_CALLBACK_URL = 'http://tron-ci.herokuapp.com/github-callback'
 GITHUB_SCOPE = 'read:org, repo:status, repo_deployment, user:email, write:repo_hook'
